08c5363b18c33d9776246d49edd45797537da90a
Skyler Ferrante <sjf5462@rit.edu>
2021-07-19 22:06:55 -0400

    Improved comments

--- sudoku.c
+++ sudoku.c
@@ -2,10 +2,23 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-//A puzzle, is a 2d array of ints, always of size 9x9
-//It can have values from 0-9 (0 being unfilled)
+/**
+ * A puzzle is a 2d array of ints, always of size 9x9
+ * A point in a puzzle can have values from 0-9 (0 being unfilled)
+ * A puzzle has nine 3x3 grids
+ * 
+ * For a puzzle to be considered solved:
+ * 	The puzzle has no empty spots
+ * 	The puzzles grids contain unique digits (1-9)
+ * 	The puzzles rows and columns contain unique digits (1-9)
+**/
 
-//Get a single puzzle
+/**
+ * Get a single puzzle
+ * 
+ * file: file containing sudoku clues, with one line per sudoku puzzle
+ * puzzle: puzzle buffer to fill with puzzle data from file 
+**/
 void get_puzzle(FILE *file, int puzzle[9][9])
 {
 	fgetc(file); //Getting rid of new line
@@ -14,7 +27,11 @@ void get_puzzle(FILE *file, int puzzle[9][9])
 			puzzle[row][col] = fgetc(file) - 48;
 }
 
-//Prints a single puzzle to stdout
+/**
+ * Prints a single puzzle to stdout
+ * 
+ * puzzle: puzzle to show
+**/
 void show_puzzle(int puzzle[9][9])
 {
 	for (int row = 0; row < 9; row++)
@@ -32,7 +49,15 @@ void show_puzzle(int puzzle[9][9])
 	}
 }
 
-//Checks row given by row_index
+/**
+ * Checks row given by row_index
+ * 
+ * puzzle: puzzle to check row
+ * row_index: number row to check
+ * 
+ * returns: true  if row  is valid
+ * 			false if row  is invalid
+**/
 bool check_row(int puzzle[9][9], int row_index)
 {
 	bool found_numbers[9] = {0}; //Initialized to zero
@@ -50,7 +75,15 @@ bool check_row(int puzzle[9][9], int row_index)
 	return true;
 }
 
-//Checks column given by column_index
+/**
+ * Checks column given by column_index
+ * 
+ * puzzle: puzzle to check column
+ * column_index: number column to check
+ * 
+ * returns: true  if column  is valid
+ * 			false if column  is invalid
+**/
 bool check_column(int puzzle[9][9], int column_index)
 {
 	bool found_numbers[9] = {0};
@@ -67,7 +100,16 @@ bool check_column(int puzzle[9][9], int column_index)
 	return true;
 }
 
-//Check grid given by row and column index (A grid is a 3x3 in sudoku)
+/**
+ * Check grid given by row and column index
+ * 
+ * puzzle: puzzle to check grid of
+ * row_index: row of grid (0-3)
+ * column_index: column of grid (0-3)
+ * 
+ * returns: true  if grid is valid
+ * 			false if grid is invalid
+**/
 bool check_grid(int puzzle[9][9], int row_index, int column_index)
 {
 	bool found_numbers[9] = {0};
@@ -93,6 +135,16 @@ bool check_grid(int puzzle[9][9], int row_index, int column_index)
 }
 
 //Check a given puzzle
+/**
+ * Check a given puzzle
+ * checks rows, columns, and grids
+ * does not check if filled
+ * 
+ * puzzle: puzzle to check
+ * 
+ * returns: true  if puzzle is valid
+ * 			false if puzzle is invalid
+**/
 bool check_sudoku(int puzzle[9][9])
 {
 	for (int i = 0; i < 9; i++)
@@ -115,7 +167,11 @@ bool check_sudoku(int puzzle[9][9])
 	return true;
 }
 
-//Check to make sure puzzle has no zeros
+/**
+ * Check to make sure puzzle has no zeros
+ * 
+ * puzzle: puzzle to check if any unfilled spots
+**/
 bool is_filled(int puzzle[9][9])
 {
 	for (int i = 0; i < 9; i++)
@@ -125,7 +181,12 @@ bool is_filled(int puzzle[9][9])
 	return true;
 }
 
-//Copy values from puzzle1 to puzzle2
+/**
+ * Copy values from puzzle1 to puzzle2
+ * 
+ * puzzle1: puzzle to copy from
+ * puzzle2: puzzle to copy to
+**/
 void copy_puzzle(int puzzle1[9][9], int puzzle2[9][9])
 {
 	for (int i = 0; i < 9; i++)
@@ -137,7 +198,13 @@ void copy_puzzle(int puzzle1[9][9], int puzzle2[9][9])
 	}
 }
 
-//Get numbers in row
+/**
+ * Get numbers in row
+ * 
+ * puzzle: puzzle to get row numbers from
+ * nums: true if number-1 is contained (zero ignored)
+ * row_index: row to get numbers from
+**/
 void get_row_nums(int puzzle[9][9], bool nums[9], int row_index)
 {
 	for (int i = 0; i < 9; i++)
@@ -150,7 +217,13 @@ void get_row_nums(int puzzle[9][9], bool nums[9], int row_index)
 	}
 }
 
-//Get numbers in col
+/**
+ * Get numbers in col
+ * 
+ * puzzle: puzzle to get col numbers from
+ * nums: true if number-1 is contained (zero ignored)
+ * col_index: col to get numbers from
+**/ 
 void get_col_nums(int puzzle[9][9], bool nums[9], int col_index)
 {
 	for (int i = 0; i < 9; i++)
@@ -164,7 +237,15 @@ void get_col_nums(int puzzle[9][9], bool nums[9], int col_index)
 }
 
 //Get numbers in grid
-void get_grid_nums(int puzzle[9][9], bool grid_numbers[9], int row_index, int column_index)
+/**
+ * Get numbers in grid
+ * 
+ * puzzle: puzzle to get grid numbers from
+ * nums: true if number-1 is contained (zero ignored)
+ * row_index: row of grid (0-3)
+ * column_index: column of grid (0-3) 
+**/
+void get_grid_nums(int puzzle[9][9], bool nums[9], int row_index, int column_index)
 {
 	row_index *= 3; //Grids are 3x3
 	column_index *= 3;
@@ -176,13 +257,23 @@ void get_grid_nums(int puzzle[9][9], bool grid_numbers[9], int row_index, int co
 			int num = puzzle[i + row_index][j + column_index];
 			if (num != 0)
 			{
-				grid_numbers[num - 1] = true;
+				nums[num - 1] = true;
 			}
 		}
 	}
 }
 
-//Find a value for row and col, that is bigger than min_value, that satisfies the given puzzle
+/**
+ * Find a value for row and col, that is bigger than min_value, that satisfies the given puzzle
+ *
+ * puzzle: puzzle to solve point on
+ * row: row of point to solve
+ * col: col of point to solve
+ * min_value: minimum value of point
+ * 
+ * returns: 0 if no possible solve over min_value
+ * 			value of the first found possible solve
+**/
 int solve_sudoku_one_point(int puzzle[9][9], int row, int col, int min_value)
 {
 	int grid_row_index = (int)row / 3;
@@ -210,8 +301,19 @@ int solve_sudoku_one_point(int puzzle[9][9], int row, int col, int min_value)
 	return 0;
 }
 
-//Solve given puzzle recursively
-//It just goes from top left, to bottom right, which is horribly inefficient
+/**
+ * Solve given puzzle recursively
+ * It just goes from top left, to bottom right, which is horribly inefficient
+ * 
+ * puzzle: puzzle to solve
+ * row: row to start
+ * col: col to start
+ * min_value: min_value for point
+ * orginal_puzzle_copy: copy of puzzle
+ * 
+ * returns: false if no solution
+ * 			true  if puzzle solved
+**/
 bool solve_sudoku_recursive(int puzzle[9][9], int row, int col, int min_value, int orginal_puzzle_copy[9][9])
 {
 	if (row == 8 && col == 9)
@@ -245,7 +347,14 @@ bool solve_sudoku_recursive(int puzzle[9][9], int row, int col, int min_value, i
 	return solution;
 }
 
-//Helper method, which calls solve_sudoku_recursive
+/**
+ * Helper method, which calls solve_sudoku_recursive on single puzzle
+ * 
+ * puzzle: puzzle to solve
+ *  
+ * returns: false if no solution
+ * 			true  if puzzle solved
+**/
 bool solve_sudoku_recursively(int puzzle[9][9])
 {
 	int orginal_puzzle_copy[9][9];
@@ -253,8 +362,12 @@ bool solve_sudoku_recursively(int puzzle[9][9])
 	return solve_sudoku_recursive(puzzle, 0, 0, 1, orginal_puzzle_copy);
 }
 
-//Solve sudoku iteratively
-//But this basically does the same thing as solve_sudoku_recusive
+/**
+ * Solve sudoku iteratively
+ * Basically does the same thing as solve_sudoku_recusive
+ * 
+ * puzzle: puzzle to solve
+**/
 bool solve_sudoku_iteratively(int puzzle[9][9])
 {
 	int orginal_puzzle_copy[9][9];
@@ -316,6 +429,13 @@ bool solve_sudoku_iteratively(int puzzle[9][9])
 	return true;
 }
 
+/**
+ * Opens sudoku file
+ * While true:
+ * 	Reads puzzle from file
+ * 	Solves puzzle
+ * 	Prints time to solve puzzle
+**/
 int main()
 {
 	FILE *file;
@@ -332,8 +452,7 @@ int main()
 		return -1;
 	}
 	printf("Solving %d number of puzzles\n", number_of_puzzles);
-	// number_of_puzzles = 10;
-
+	
 	int puzzle[9][9];
 
 	clock_t start_of_program = clock();
@@ -354,6 +473,8 @@ int main()
 		double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
 		printf("Time spent: %f\n", time_spent);
 		printf("CHECK: %s\n\n", check_sudoku(puzzle) ? "PASS" : "FAIL");
+		
+		number_of_puzzles = 0;
 	}
 
 	clock_t end_of_program = clock();
