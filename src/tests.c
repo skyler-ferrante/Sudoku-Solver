0ed06881da4b71536ba5a717f45dce53d218ff72
skyler-ferrante <sjf5462@rit.edu>
2022-03-13 23:02:59 -0400

    Added iterative and clock to test
    Used to only test recursive

--- src/tests.c
+++ src/tests.c
@@ -1,5 +1,9 @@
 #include<assert.h>
+#include<time.h>
+#include<string.h>
 #include "sudoku.h"
+
+void print_time(clock_t start, clock_t end);
 
 int main(){
 	int puzzle[9][9] = {
@@ -13,9 +17,26 @@ int main(){
 		{0,5,0,1,0,0,0,0,0,},
 		{0,0,0,8,0,6,0,0,0,}
 	};
-	
-	//show_puzzle(puzzle);
-	solve_sudoku_recursively(puzzle);
+
+	int recursive_solution[9][9];
+	int iterative_solution[9][9];
+
+	memcpy(recursive_solution, puzzle, sizeof(puzzle));
+	memcpy(iterative_solution, puzzle, sizeof(puzzle));
+
+	printf("Running recursive\n");
+
+	clock_t start_recursive = clock();
+	solve_sudoku_recursively(recursive_solution);
+	clock_t end_recursive = clock();
+	print_time(start_recursive, end_recursive);
+
+	printf("Running iterative\n");
+
+	clock_t start_iterative = clock();
+	solve_sudoku_iteratively(iterative_solution);
+	clock_t end_iterative = clock();
+	print_time(start_iterative, end_iterative);
 
 	int correct_solution[9][9] = {
 		{6,9,3,7,8,4,5,1,2},
@@ -29,16 +50,23 @@ int main(){
 		{2,7,4,8,3,6,1,5,9}
 	};
 
-	int puzzle_num, solution_num;
+	int iterative_num, recursive_num, solution_num;
 
 	for(int i = 0; i < 9; i++){
 		for(int j = 0; j < 9; j++){
-			puzzle_num = puzzle[i][j];
+			iterative_num = iterative_solution[i][j];
+			recursive_num = recursive_solution[i][j];
 			solution_num = correct_solution[i][j];
 
-			assert(puzzle_num == solution_num);
+			assert(iterative_num == solution_num);
+			assert(recursive_num == solution_num);
 		}
 	}
 
 	printf("Tests passed!\n");
+}
+
+void print_time(clock_t start, clock_t end){
+	double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+	printf("Time spent: %f\n", time_spent);
 }
